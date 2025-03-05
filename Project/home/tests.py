e7787b4dae814719b2ea92a4730127eb5f71d454
Dalton Fowler <dalton.jfowler@yahoo.com>
2025-03-05 18:01:34 +0000

    added fake tests to run to test workflow

--- Project/home/tests.py
+++ Project/home/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class FakeTestCase(TestCase):
+    def test_assertion(self):
+        self.assertEqual(type(1), int)
+        self.assertEqual(type("hello"), str)
