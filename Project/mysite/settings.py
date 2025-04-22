61bebc556e7254544f85b1685d03be430dc189f0
Dalton Fowler <dalton.jfowler@yahoo.com>
2025-04-22 00:46:23 +0000

    updating settings

--- Project/mysite/settings.py
+++ Project/mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -136,7 +136,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #
-FORCE_SCRIPT_NAME = "/proxy/3000"
+#FORCE_SCRIPT_NAME = "/proxy/3000"
 
 # Use x fowarded host header to determine original host
 USE_X_FORWARDED_HOST = True
