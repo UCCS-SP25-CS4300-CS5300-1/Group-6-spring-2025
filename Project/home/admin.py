815f829c771fb8b00c17740c7d0666373ef1f80c
Maxwell Hymer <mhymer@uccs.edu>
2025-05-03 02:50:58 +0000

    did some more linting

--- Project/home/admin.py
+++ Project/home/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+"""
+This module is used to register models for the Django admin interface.
+Currently, no models are registered.
+"""
+#from django.contrib import admin
 
 # Register your models here.
--- Project/home/apps.py
+++ Project/home/apps.py
@@ -1,6 +1,16 @@
+"""
+This module contains the configuration for the 'home' app in the Django project.
+It defines the HomeConfig class, which configures settings for the 'home' app.
+"""
+
 from django.apps import AppConfig
 
 
 class HomeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'home'
+    """
+    Configuration class for the 'home' app. It defines the default auto field
+    type and the name of the app.
+    """
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "home"
