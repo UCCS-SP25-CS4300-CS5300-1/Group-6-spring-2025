5b1cb53df77405b310efb4081ff7db3d27700d37
Maxwell Hymer <mhymer@uccs.edu>
2025-05-05 00:57:43 +0000

    touching up final home and account pylint changes

--- Project/home/context_processors.py
+++ Project/home/context_processors.py
@@ -15,3 +15,4 @@ def base_url(request):
     return {
         'base_url': f"{request.scheme}://{request.get_host()}/proxy/8000/"
     }
+    
--- Project/home/urls.py
+++ Project/home/urls.py
@@ -9,7 +9,7 @@ from django.urls import path
 from .views import index, generate_workout, calendar_view, workout_events, save_to_calendar
 
 urlpatterns = [
-    path('', index, name='home'),  
+    path('', index, name='home'), 
     path('generate-workout/', generate_workout, name='generate_workout'),
     path('calendar/', calendar_view, name='calendar'),
     path('workout-events/', workout_events, name='workout_events'),
